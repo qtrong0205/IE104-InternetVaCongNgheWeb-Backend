f80857cdb11acc7eb4d57b218fb3d669ef77fb5a
qtrong0205 <quoctrong02052006@gmail.com>
2026-09-22 09:25:49 +0700

    initial project

---
+++ src/main/java/com/nhom6/motofix/MotofixApplication.java
@@ -0,0 +1,13 @@
+package com.nhom6.motofix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotofixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotofixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhom6/motofix/MotofixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom6.motofix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotofixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
